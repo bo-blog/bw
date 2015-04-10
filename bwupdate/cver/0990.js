f031147777faf264d133e5e420bc4e5749aaa245
bo-blog <info@bo-blog.com>
2015-04-10 17:39:09 +0800

    Update 0990.js

--- bwupdate/cver/0990.js
+++ bwupdate/cver/0990.js
@@ -1,2 +1,1 @@
-$("#ins").html("有新版本可用：<b>0.9.9</b><br/>- 新增：后台可上传替换头像。<br/>- 新增：可支持Apache的伪静态（URL重写）。<br/>- 新增：安装程序多语言化，包含中文。<br/>- 新增：繁体中文语言包。<br/>- 暂时取消了备份功能，以待进一步完善。");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg0980.zip", '856A3A90727B25E4DCCE80BFFE9F8785');
+$("#ins").html("暂无新版本可用。");
