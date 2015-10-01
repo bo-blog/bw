720a7c3a009e15491ef74775d3448b3855d56396
bo-blog <info@bo-blog.com>
2015-10-01 16:15:28 +0800

    Update v1000.js

--- bwupdate/cver/v1000.js
+++ bwupdate/cver/v1000.js
@@ -1,2 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.0.0</b><br/>- Test");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1000.txt", '856A3A90727B25E4DCCE80BFFE9F8785');
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1000.txt", '8d2faa8eada6a167ab91c4b62454b924');
