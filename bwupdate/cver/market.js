fd0e61f77c1e1dd262c7c6703868b2a5e3dcff9e
bo-blog <info@bo-blog.com>
2015-10-05 15:42:27 +0800

    Create market.js

---
+++ bwupdate/cver/market.js
@@ -0,0 +1,9 @@
+
+var themeList=[{"tid":"tp10001a","tn":"\u6d4b\u8bd5\u4e3b\u98981","ta":"\u6d4b\u8bd5\u8005","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test2.jpg","tl":"#"},{"tid":"tp10002a","tn":"\u6d4b\u8bd5\u4e3b\u98982","ta":"\u6d4b\u8bd5\u8005","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test3.jpg","tl":"#"}];
+var extList=[{"tid":"ep10001a","tn":"\u6d4b\u8bd5\u6269\u5c551","td":"\u7b2c\u4e09\u65b9\u5206\u4eab","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test4.jpg","tl":"#"},{"tid":"ep10002a","tn":"\u6d4b\u8bd5\u6269\u5c552","td":"\u9875\u9762\u529f\u80fd","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test5.jpg","tl":"#"},{"tid":"ep10003a","tn":"\u6d4b\u8bd5\u6269\u5c553","td":"\u540e\u53f0\u7ba1\u7406","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test6.jpg","tl":"#"}];
+var heroList=[{"tid":"h1","tthm":"http:\/\/7fvhw4.com1.z0.glb.clouddn.com\/test1.jpg","tl":"#"}];
+var bulletinList=[{"tid":"news1","tn":"\u6211\u4eec\u7684\u5e02\u573a\u5c1a\u672a\u5f00\u901a\u3002\u656c\u8bf7\u671f\u5f85\u3002(2015\/10\/5)","tl":"#"},{"tid":"news2","tn":"\u4f7f\u7528\u4e2d\u9047\u5230\u4ec0\u4e48\u95ee\u9898\uff0c\u6b22\u8fce\u5230GitHub\u53cd\u9988\u3002(2015\/10\/2)","tl":"#"},{"tid":"news3","tn":"\u795d\u60a8\u56fd\u5e86\u5feb\u4e50\u3002(2015\/10\/1)","tl":"#"}];
+parseInfo (themeList, 'theme');
+parseInfo (extList, 'extension');
+parseInfo (heroList, 'hero');
+parseInfo (bulletinList, 'bulletin');
