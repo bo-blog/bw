da0dd038e49388c1ed8b0ad70279ecf179a2a2e3
bo-blog <info@bo-blog.com>
2015-12-05 15:28:53 +0800

    Update v1000.js

--- bwupdate/cver/v1000.js
+++ bwupdate/cver/v1000.js
@@ -1,2 +1,7 @@
-$("#ins").html("有新版本可用：<b>1.0.0</b><br/>- Test");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1000.txt", '8d2faa8eada6a167ab91c4b62454b924');
+$("#ins").html("有新版本可用：<b>1.0.1</b><br/>
+- 修复：写帖子页面在IE10和Edge下无响应。<br/>
+- 改进：提高了撰写时自动提示tag功能的效率。<br/>
+- 新增：编辑器可以插入优酷和虾米的视频/音乐了。<br/>
+- 新增：为编辑器载入增加了一个接口，留待以后增加其他编辑器使用。<br/>
+- “市场”暂时被屏蔽了，直到正式开发完成为止。<br/>- Test");
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1000.txt", '65b91a9393198a8bffb8d35174d287ca');
