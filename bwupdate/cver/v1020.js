bc60e62c786feb2b235c69c3574a22be2eb1fc7b
bo-blog <info@bo-blog.com>
2016-01-04 23:25:55 +0800

    Update v1020.js

--- bwupdate/cver/v1020.js
+++ bwupdate/cver/v1020.js
@@ -1,1 +1,2 @@
-$("#ins").html("当前版本为最新。<br/>");
+$("#ins").html("有新版本可用：<b>1.0.3</b><br/>- 修复：更新了第三方markdown类库，现已兼容PHP 7。<br/>- 新增：可通过专用安装包自动安装第三方主题和扩展。<br/>- 新增：社交网络链接补全了推特和FB。<br/>- 新增：内置社会化评论系统现在支持Disqus。<br/>- 修复：Error页面“返回前页”链接为空的bug。<br/>- 新增：支持插入网易云音乐播放器。<br/>- 可以正常运行在设置过 path_info 的 nginx 下了。<br/>- 一些为静态生成项目Mill提供支持的小改动。<br/>");
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1020.txt", '78eb9925362bf45ca099f379c1e0b6b7');
