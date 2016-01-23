f4554809508eb7860d2dc47576197a5a21b3dcb6
bo-blog <info@bo-blog.com>
2016-01-23 20:01:39 +0800

    Update v1030.js

--- bwupdate/cver/v1030.js
+++ bwupdate/cver/v1030.js
@@ -1,1 +1,2 @@
-$("#ins").html("当前版本为最新。<br/>");
+$("#ins").html("有新版本可用：<b>1.0.4</b><br/>- 修复：正文内页分类错误。<br/>- 修复：引用和代码无法换行。<br/>- 修复：插入虾米音乐时重复插入。<br/>- 合并为第一个Master版本。<br/>");
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1030.txt", 'ff29d4216bc18739ad1393bef703617a');
