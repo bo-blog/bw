988a34514b73d5fa60627aa4fe4b95b9ce462562
bo-blog <info@bo-blog.com>
2016-04-17 13:58:17 +0800

    Update v1060.js

--- bwupdate/cver/v1060.js
+++ bwupdate/cver/v1060.js
@@ -1,2 +1,2 @@
 $("#ins").html("有新版本可用：<b>1.0.7</b><br/>- 修复：解析器单回车换行。<br/>- 新增：可以为每个分类设置单独的主题。<br/>");
-addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1060.txt", '3703fed49d88028f36413138b4023729');
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1060.txt", '8d120e58e14302b15361e0ab39a0fd60');
