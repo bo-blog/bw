3229912b972dd4b8b60b9bffddae1c8a12382714
bo-blog <info@bo-blog.com>
2016-05-29 20:49:21 +0800

    Update v1070.js

--- bwupdate/cver/v1070.js
+++ bwupdate/cver/v1070.js
@@ -1,1 +1,2 @@
-$("#ins").html("当前版本为最新。<br/>");
+$("#ins").html("有新版本可用：<b>1.0.8</b><br/>- 改进：可以在文章管理页面搜索文章。<br/>- 改进：分离了前后台的 css 文件，减少前台载入体积。<br/>- 改进：上传文件到七牛后保留原扩展名。<br/>- 新增：给出了一个nginx用的 URL rewrite 模板。<br/>- 一些手机浏览时的细节调整。<br/>");
+addUpdateBtn ("http://xiaofengtest.qiniudn.com/upkg1070.txt", 'ac08385153e39fa75e9aa7cdf5ec6318');
