dd5add177268a5f7cb38abf242fbbac861403ebd
bo-blog <info@bo-blog.com>
2018-08-23 23:09:31 +0800

    v1.6.0

--- inc/script/xiami.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (isset ($_GET['xmID']))
-{
-	die ("<body><script type=\"text/javascript\" src=\"http://www.xiami.com/widget/player-single?uid=0&sid=".floor($_GET['xmID'])."&mode=js\"></script></body>");
-}
